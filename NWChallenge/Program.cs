049694708cda57bb7b5cad665beae403e71223c7
Naufaldy Rooswandy <naufaldymdrp@outlook.com>
2024-04-28 16:27:01 +0700

    Added case 2 work

--- NWChallenge/Program.cs
+++ NWChallenge/Program.cs
@@ -8,18 +8,18 @@ public class Program
         // Problem 1: Sort Characters
         
         // Input 1
-        Console.Write("Input one line of words (S) : ");
-        string? problem1Input1 = Console.ReadLine();
-        SortCharacters(problem1Input1 ?? string.Empty, out OrderedDictionary vowels, out OrderedDictionary consonants);
-        PrintCharacters("Vowel Characters", vowels);
-        PrintCharacters("Consonants Characters", consonants);
-        
-        // Input 2
-        Console.Write("Input one line of words (S) : ");
-        string? problem1Input2 = Console.ReadLine();
-        SortCharacters(problem1Input2 ?? string.Empty, out OrderedDictionary vowels2, out OrderedDictionary consonants2);
-        PrintCharacters("Vowel Characters", vowels2);
-        PrintCharacters("Consonants Characters", consonants2);
+        // Console.Write("Input one line of words (S) : ");
+        // string? problem1Input1 = Console.ReadLine();
+        // SortCharacters(problem1Input1 ?? string.Empty, out OrderedDictionary vowels, out OrderedDictionary consonants);
+        // PrintCharacters("Vowel Characters", vowels);
+        // PrintCharacters("Consonants Characters", consonants);
+        // 
+        // // Input 2
+        // Console.Write("Input one line of words (S) : ");
+        // string? problem1Input2 = Console.ReadLine();
+        // SortCharacters(problem1Input2 ?? string.Empty, out OrderedDictionary vowels2, out OrderedDictionary consonants2);
+        // PrintCharacters("Vowel Characters", vowels2);
+        // PrintCharacters("Consonants Characters", consonants2);
 
         // --
 
@@ -27,13 +27,60 @@ public class Program
         
         // Input 1
         Console.Write("Input the number of families : ");
-        string? problem2Input1 = Console.ReadLine();
+        string problem2Input1Line1 = Console.ReadLine() ?? string.Empty;
+
+        bool result = int.TryParse(problem2Input1Line1, out int familyCount);
+        if (!result)
+        {
+            Console.WriteLine("Oops error, input is not number");
+        }
+
+        Console.Write("Input the number of members in the family (separated by a space) : ");
+        string problem2Input1Line2 = Console.ReadLine() ?? string.Empty;
+
+        string strResult = CountBusDuringQuarantine(problem2Input1Line2, familyCount);
+        Console.WriteLine(strResult);
 
         // Input 2
 
     }
 
     // --
+
+    public static string CountBusDuringQuarantine(string input, int familyCount)
+    {
+        string[] splits = input.Split(' ');
+
+        if (splits.Length != familyCount) 
+        {
+            return "Input must be equal with count of family";
+        }
+
+        int[] familyMemberCounts = new int[familyCount];
+        for (int i = 0; i < familyCount; i++)
+        {
+            if (!int.TryParse(splits[i], out int familyMemberCount))
+            {
+                return "Error in converting number from string split";
+            }
+
+            familyMemberCounts[i] = familyMemberCount;
+        }
+
+        int sum = 0;
+        foreach (int count in familyMemberCounts)
+        {
+            sum += count;
+        }
+
+        int busCount = sum / 4;
+
+        bool additionalBus = sum % 4 != 0;
+        if (additionalBus) busCount += 1;
+
+        
+        return $"Minimum bus required is : {busCount}";
+    }
 
     // -------------------------------------------------------------------------
     // -------------------------------------------------------------------------
